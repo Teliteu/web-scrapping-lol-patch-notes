79ffbd56da9c4e2788150ff750be61553aa6206c
Mateus Adriano <mateus.opinheiro@gmail.com>
2021-06-03 01:53:08 -0300

    Update import.py
    Start insert data on database

--- import.py
+++ import.py
@@ -1,3 +1,8 @@
+import pymysql
+
+conex = pymysql.connect(host="localhost", user="root", password="", db="mydb")
+cuq=conex.cursor()
+
 import requests
 from bs4 import BeautifulSoup, element
 import datetime
@@ -6,8 +11,8 @@ number=int(input("Number of links?: "))
 
 
 while (number!=0):
-
      url = input("Link please: ")
+
 
      req = requests.get(url)
 
@@ -23,11 +28,15 @@ while (number!=0):
 
      for lista_campp in lista_camp:
           lista = lista_campp.find_all("a",href_="")
-          for lista_camppp in lista:
-              for lista_campppp in lista_camppp.strings:
-                print(lista_campppp)
+          for lista_camppp in lista: #precisa fazer para transformar em objeto novamente
+               for lista_campppp in lista_camppp.strings:
+                    cuq.execute("INSERT INTO testee(Nova) VALUES (%s)", lista_campppp) #excutemany seria um letra por vez
+                    print(lista_campppp)
+
+                
      number=number-1
+     conex.commit()
+
 Else: print("Finish baby!")
 
-
-
+conex.close()

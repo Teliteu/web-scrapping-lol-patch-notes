e53b9b6919135eb926ac50574820b80685f82719
Mateus Adriano <mateus.opinheiro@gmail.com>
2021-06-24 23:47:55 -0300

    News
    I discovered a best way to scrap and this time i store data direct in lists. Im commiting the other code with the old scrap that connect with SQL

--- import.py
+++ import.py
@@ -1,11 +1,7 @@
-import pymysql
 
-conex = pymysql.connect(host="localhost", user="root", password="", db="mydb")
-cuq=conex.cursor()
-
+import re
 import requests
 from bs4 import BeautifulSoup, element
-import datetime
 
 number=int(input("Number of links?: "))
 
@@ -13,30 +9,20 @@ number=int(input("Number of links?: "))
 while (number!=0):
      url = input("Link please: ")
 
-
      req = requests.get(url)
 
      soup = BeautifulSoup(req.content,"lxml")
 
-     dateTime = soup.select("time")[0]["datetime"]
-     print(dateTime)
-
      note=soup.find("title")
-     print(note.next_element)
+     note=note.next_element
+     note=str.split(note)
+     note3=note[3]
+     print(note3)
 
-     lista_camp = soup.find_all("div",class_="patch-change-block white-stone accent-before")
-
-     for lista_campp in lista_camp:
-          lista = lista_campp.find_all("a",href_="")
-          for lista_camppp in lista: #precisa fazer para transformar em objeto novamente
-               for lista_campppp in lista_camppp.strings:
-                    cuq.execute("INSERT INTO testee(Nova) VALUES (%s)", lista_campppp) #excutemany seria um letra por vez
-                    print(lista_campppp)
-
-                
+     lista = soup.find_all(string=["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", "Aurelion Sol", "Azir", "Bardo", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "Cho'Gath", "Corki","Darius", "Diana", "Dr. Mundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx", "Kalista", "Kai'Sa", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Kha'Zix", "Kindred", "Kled", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lillia", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Neeko", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "Rek'Sai", "Rell", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Samira", "Sejuani", "Senna", "Seraphine", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Sylas", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vi", "Viego", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra"])
+    
+     print(lista)
+              
      number=number-1
-     conex.commit()
-
 Else: print("Finish baby!")
 
-conex.close()
---
+++ scrap_to_SQL.py
@@ -0,0 +1,42 @@
+import pymysql
+
+conex = pymysql.connect(host="localhost", user="root", password="", db="mydb")
+cuq=conex.cursor()
+
+import requests
+from bs4 import BeautifulSoup, element
+import datetime
+
+number=int(input("Number of links?: "))
+
+
+while (number!=0):
+     url = input("Link please: ")
+
+
+     req = requests.get(url)
+
+     soup = BeautifulSoup(req.content,"lxml")
+
+     dateTime = soup.select("time")[0]["datetime"]
+     print(dateTime)
+
+     note=soup.find("title")
+     print(note.next_element)
+
+     lista_camp = soup.find_all("div",class_="patch-change-block white-stone accent-before")
+
+     for lista_campp in lista_camp:
+          lista = lista_campp.find_all("a",href_="")
+          for lista_camppp in lista: 
+               for lista_campppp in lista_camppp.strings:
+                    cuq.execute("INSERT INTO testee(Nova) VALUES (%s)", lista_campppp) #excutemany seria um letra por vez
+                    print(lista_campppp)
+
+                
+     number=number-1
+     conex.commit()
+
+Else: print("Finish baby!")
+
+conex.close()
